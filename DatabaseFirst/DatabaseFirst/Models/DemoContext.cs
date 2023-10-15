629faee5c03c6d4921983c5382cc8d5c96f710ee
Lushen Moodley <lushenmoodley@yahoo.com>
2023-10-15 13:35:16 +0200

    all files are here to load the project

---
+++ DatabaseFirst/DatabaseFirst/Models/DemoContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseFirst.Models;
+
+public partial class DemoContext : DbContext
+{
+    public DemoContext()
+    {
+    }
+
+    public DemoContext(DbContextOptions<DemoContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=LUSHEN_LAPTOP\\SQLEXPRESS;initial catalog=Demo;TrustServerCertificate=True;trusted_connection=yes;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.ToTable("Employee");
+
+            entity.Property(e => e.Firstname)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+            entity.Property(e => e.Lastname)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
